a878761c7e62f19e266e95efc8397e240fe9dd5d
Dario Berzano <dario.berzano@cern.ch>
2015-04-01 14:32:47 +0200

    Condor: fixed script to run CernVM container

--- my_run.sh
+++ my_run.sh
@@ -28,12 +28,11 @@ cat "${prog_dir}/condor-configure" |
 
 # See here[1] for the swap
 # https://github.com/docker/docker/blob/master/docs/sources/reference/run.md
-echo exec docker run -i -t \
+exec docker run -i -t \
   -v /cvmfs/alice.cern.ch:/cvmfs/alice.cern.ch \
-  -v ${prog_dir}/condor-configure:/condor-configure \
+  -v ${prog_dir}/condor-configure.ok:/condor-configure \
   -v ${prog_dir}/condor-hook-job-exit:/condor-hook-job-exit \
   -v ${prog_dir}/proc-cpuinfo-1cpu:/proc/cpuinfo \
-  -m 2g \
   --cpu-shares=$ncpushares \
   --memory=$mem_share_bytes \
   --memory-swap=0 \
